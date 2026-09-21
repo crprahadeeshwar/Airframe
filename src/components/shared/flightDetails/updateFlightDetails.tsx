a5e6291407ee3942e59a0608e14559c740569e2a
Prahad <cr.prahadeeshwar@gmail.com>
2026-09-21 19:44:39 +0530

    Created Update Flight Card file.

---
+++ src/components/shared/flightDetails/updateFlightDetails.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from "../../ui/button"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../ui/card"
+import { ArrowRight, Plane } from "lucide-react"
+import updateFlight from '@/src/features/actions/updateFlights'
+
+
+export default function updateFlightDetails() {
+  return (
+    <div>update Flight Details</div>
+  )
+}
+
